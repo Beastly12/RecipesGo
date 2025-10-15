a593bd2c148bdf5b3d4dcbba7f5600b3f51c271d
Zhihan Liu <2LIUZ98@solent.ac.uk>
2025-10-15 22:59:24 +0100

    profile page basic outline done

--- RecipesGo/src/profile.jsx
+++ RecipesGo/src/profile.jsx
@@ -3,17 +3,56 @@ import React, { useState } from "react";
 import "./profile.css"; 
 
 export default function Profile() {
+    const log = () => alert("Logged In Successful");
+  const signup = () => alert("Account Created Successfully");
+
 
   return (
     <div className="profile-container">
-      <div className="picture-block">
-        <h2>Username</h2>
-      </div>
+       <div className="picture-block">
+            <h1>Username</h1>
+        </div>
 
-      <div className="details-container">
-        <h2>USERNAME</h2>
-        <p>BIO</p>
-      </div> 
+        <div className="details-container">
+            <h2>USERNAME</h2>
+            <p>BIO</p>
+
+            
+                <div className="stats">
+                    <div className="recipes">
+                        <h2>10</h2>
+                        <h5>Recipes</h5>
+                    </div>
+                    <div className="likes">
+                        <h2>12K</h2>
+                        <h5>Likes</h5>
+                    </div>
+
+                </div>
+
+                <div className="settings">
+                    <button onClick={signup}>Edit Profile</button>
+                    <button onClick={signup}>Settings</button>
+
+                </div>
+
+
+        </div>
+          
+
+         
     </div>
+
+      
   );
 }
+
+
+
+
+    
+
+   
+
+
+  
