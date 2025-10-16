dbf579a726f8b9a73e2383b5b6b5158b470fb1b9
Daniel <danieladeyd77@gmail.com>
2025-10-16 16:58:48 +0100

    first commit

---
+++ recipesgo/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <hi>App</hi>
+    </div>
+  );
+}
+
+export default App;
---
+++ recipesgo/src/componets/authpage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function authpage() {
+  <div>
+    <h1>Authpage</h1>
+  </div>;
+}
+
+export default authpage;
