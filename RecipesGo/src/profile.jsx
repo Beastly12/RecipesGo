c43126ec60104cf48896af6cec10efbb837b136f
Zhihan Liu <2LIUZ98@solent.ac.uk>
2025-10-16 16:17:32 +0100

    starting to create the profile page's recipes and favorites area

--- RecipesGo/src/profile.jsx
+++ RecipesGo/src/profile.jsx
@@ -3,50 +3,47 @@ import React, { useState } from "react";
 import "./profile.css"; 
 
 export default function Profile() {
-  const signup = () => alert("Account Created Successfully");
+  const edit = () => alert("Settings Edit");
 
 
   return (
-    <div className="profile-container">
-       <div className="picture-block">
-       <img 
-    src="\profile.webp" 
-    alt="Profile" 
-    className="profile-picture" 
-  />
-        </div>
+    <div className="full-container">
+        <div className="profile-container">
+           <div className="picture-block">
+               <img 
+                src="\profile.webp" 
+                alt="Profile" 
+                className="profile-picture" />
+            </div>
 
-        <div className="details-container">
-            <h1>USERNAME</h1>
-            <p>BIO</p>
-
+            <div className="details-container">
+                <h1>USERNAME</h1>
+                <p>BIO</p>
             
-                <div className="stats">
-                    <div className="recipes">
-                        <h2>10</h2>
-                        <h5>Recipes</h5>
+                    <div className="stats">
+                        <div className="recipes">
+                            <h2>10</h2>
+                            <h5>Recipes</h5>
+                        </div>
+                        <div className="likes">
+                            <h2>12K</h2>
+                            <h5>Likes</h5>
+                        </div>
                     </div>
-                    <div className="likes">
-                        <h2>12K</h2>
-                        <h5>Likes</h5>
+
+                    <div className="settings">
+                        <button onClick={edit}>Edit Profile</button>
+                        <button onClick={edit}>Settings</button>
+
                     </div>
-
-                </div>
-
-                <div className="settings">
-                    <button onClick={signup}>Edit Profile</button>
-                    <button onClick={signup}>Settings</button>
-
-                </div>
-
-
+            </div>
         </div>
-          
 
-         
+        <div className="recipies">
+            <h2>My Recipies</h2>
+            <h5>Favorites</h5>
+        </div>
     </div>
-
-      
   );
 }
 
