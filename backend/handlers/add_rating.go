21f727379649280aa34e8237b128d28ae49742b8
“david <davzy007@gmail.com>
2025-10-20 10:27:51 +0100

    jhkgjfukgfcfh

---
+++ backend/handlers/add_rating.go
@@ -0,0 +1,35 @@
+package handlers
+
+import (
+	"backend/models"
+	"backend/utils"
+	"context"
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+type AddRatingDependencies struct {
+	Dependencies utils.DynamoAndCloudfront
+}
+
+type ratingBody struct {
+	Rating  float32 `json:"rating"`
+	Comment string  `json:"comment"`
+}
+
+func (this *AddRatingDependencies) HandleAddRatings(ctx context.Context, req *events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var reqBody ratingBody
+	if err := json.Unmarshal([]byte(req.Body), &reqBody); err != nil {
+		log.Println("failed to unmarshal rating request body")
+		return models.InvalidRequestErrorResponse(""), nil
+	}
+
+	userid := utils.GetAuthUserId(req)
+	if userid == "" {
+		return models.UnauthorizedErrorResponse("You need to be logged in to use this feature!"), nil
+	}
+
+	return models.NotFoundResponse("Resource not implemented"), nil
+}
