e0f4919c0b34b6c234525d3e3fdd384fe07ba07a
Daniel <danieladeyd77@gmail.com>
2025-10-20 10:43:20 +0100

    feat: RecipeDetails page updated

--- recipesgo/src/App.jsx
+++ recipesgo/src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import RecipeDetailPage from "./pages/RecipeDetails";
-import AuthPage from "./pages/AuthPage";
 
 function App() {
   return (
     <Routes>
-      <Route path="/Auth" element={<AuthPage />} />
       <Route path="/recipe-details" element={<RecipeDetailPage />} />
     </Routes>
   );
