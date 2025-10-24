34f190669a7ca6087112aa61af7f8a16850b512c
Daniel <danieladeyd77@gmail.com>
2025-10-24 01:04:32 +0100

    AuthPage: Auth form in progress

--- frontend/src/views/AuthPage.jsx
+++ frontend/src/views/AuthPage.jsx
@@ -44,21 +44,67 @@ const AuthPage = () => {
         <div className=" flex flex-row justify-center items-center mb-10 md:mb-16">
           <CookingPot className=" w-40 h-20 m-2 md:w-40 md:h-30 pr-7 text-[#FF6B6B]" />
           <h1 className=" font-bold text-5xl md:text-5xl text-gray-800">
-            {" "}
             Prepify
           </h1>
         </div>
 
         {/* Toogle Buttons */}
-        <div className="flex items-center border border-gray-300 rounded-xl p-1 overflow-hidden w-full mt-10 h-15 mb-6">
-          <button className="w-1/2 py-2 text-[#FF6B6B] font-semibold bg-white">
+        <div className="flex items-center border border-gray-300 rounded-3xl p-1 overflow-hidden w-full mt-10 h-15 mb-6">
+          <button
+            onClick={() => setIsLogin(true)}
+            className={`w-1/2 py-2 text-[#FF6B6B] font-semibold transition ${
+              isLogin
+                ? "text-[#FF6B6B] bg-white border-1 border-gray-500 rounded-4xl "
+                : "text-gray-500 hover:text-[#FF6B6B]"
+            }}`}
+          >
             Login
           </button>
-          <button>Sign Up</button>
+
+          <button
+            onClick={() => setIsLogin(false)}
+            className={`w-1/2 py-2 font-semibold transition ${
+              !isLogin
+                ? "text-[#FF6B6B] bg-white border-1 border-gray-500 rounded-4xl"
+                : "text-gray-500 hover:text-[#FF6B6B]"
+            }`}
+          >
+            Sign Up
+          </button>
         </div>
 
         {/* Form */}
-        <div></div>
+        {isLogin ? (
+          <form className="space-y-7 w-full mt-4 p-3 ">
+            <div>
+              <label className="block text-2xl font-medium text-gray-700 mb-1">
+                Email
+              </label>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="w-full border border-gray-300 rounded-2xl px-5 py-3 focus:outline-none focus:ring-2 focus:ring-[#FF6B6B]"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label className="block text-2xl font-medium text-gray-700 mb-1">
+                Password
+              </label>
+              <input
+                type="email"
+                placeholder="Enter your password"
+                className="w-full border border-gray-300 rounded-2xl px-5 py-3 focus:outline-none focus:ring-2 focus:ring-[#FF6B6B]"
+              />
+
+              <button className=" self-end mt-9 cursor-pointer text-[#FF686B] font-medium hover:underline transition-colors duration-200 hover:text-[#ff4a4a]">
+                Forget password?
+              </button>
+            </div>
+          </form>
+        ) : (
+          <form className="space-y-6"></form>
+        )}
       </div>
     </div>
   );
