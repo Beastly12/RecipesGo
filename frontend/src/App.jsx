76332de6fb90ac4097e8dd3c27a706c953aea73e
Beastly12 <otudjedavid@gmail.com>
2025-10-27 11:23:41 -0700

    created profile settings page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import AuthPage from "./views/AuthPage";
 import RecipeFeed from "./views/RecipesFeed";
 import CreateRecipePage from "./views/CreateRecipePage";
 import Profile from "./views/ProfilePage";
+import ProfileSettings from "./views/ProfileSettings";
 
 export default function App() {
   return (
@@ -14,6 +15,11 @@ export default function App() {
       <Route path="/Auth" element={<AuthPage />} />
       <Route path="/" element={<RecipeFeed />} />
       <Route path="/profile" element={<Profile />} />
+
+      {/* <Route path="/" element={<h1>Home Page</h1>} /> */}
+      {/* <Route path="/login" element={<LoginSignUp />} /> */}
+      <Route path="/Settings" element={<ProfileSettings />} />
+      {/* <Route path="/recipe-details" element={<RecipeDetailPage />} /> */}
       <Route path="/createRecipe" element={<CreateRecipePage />} />
     </Routes>
   );
