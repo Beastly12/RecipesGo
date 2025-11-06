57739e5e0e80716d7929f3d72dac2381577de1b2
Daniel <danieladeyd77@gmail.com>
2025-11-06 17:21:42 +0000

    Dashboard Page still in progress

--- frontend/src/components/DashBoardManagementTable.jsx
+++ frontend/src/components/DashBoardManagementTable.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Search } from "lucide-react";
 
+const recipesData = [
+  {
+    id: 1,
+    name: "Creamy Mushroom Pasta",
+    category: "Italian",
+    meal: "Dinner",
+    time: "30 mins",
+    status: "Published",
+    date: "2 days ago",
+  },
+  {
+    id: 2,
+    name: "Homemade Pizza Dough",
+    category: "Italian",
+    meal: "Dinner",
+    time: "2 hours",
+    status: "Private",
+    date: "1 week ago",
+  },
+];
 
 const DashBoardManagementTable = () => {
-  return <section className=" rounded-3xl shadow mt-10"> </section>;
+  const [searchRecipe, SetSearchRecipe] = useState("");
+  const [recipe, setRecipe] = useState(recipesData);
+
+  return (
+    <section className=" bg-white rounded-3xl shadow mt-10">
+      <div className=""></div>
+    </section>
+  );
 };
 
 export default DashBoardManagementTable;
