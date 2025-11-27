8a4e60a860914c105e9e73a864dfa5cf7da2875b
Zhihan Liu <2LIUZ98@solent.ac.uk>
2025-11-27 16:40:23 +0000

    COMPONENT BOX.JSX?

--- frontend/src/components/ComponentBox.jsx
+++ frontend/src/components/ComponentBox.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-// import Modal from 'react-modal';
+import Modal from 'react-modal';
 import { useEffect } from 'react';
 
-// Modal.setAppElement('#root');
+Modal.setAppElement('#root');
 
 const CommentBox = ({
   totalComments,
