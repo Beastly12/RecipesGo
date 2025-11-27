addfbfa1fd82700baaec7f3455ef28b9ba55abb7
“david <davzy007@gmail.com>
2025-11-27 11:39:49 +0000

    added cypress e2e tests

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   component: {
     devServer: {
-      framework: 'react',
-      bundler: 'vite',
+      framework: "react",
+      bundler: "vite",
     },
+  },
+  e2e: {
+    env: {
+      API_URL: 'https://opsa8ce501.execute-api.eu-west-2.amazonaws.com/prod',
+      api: 'https://opsa8ce501.execute-api.eu-west-2.amazonaws.com/prod',
+    },
+    setupNodeEvents(on, config) {},
   },
 });
